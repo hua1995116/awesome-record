77cb08547c4d3e1c17fddf7477ca9d6b3f1bb869
huayifeng <461249104@qq.com>
2019-04-01 23:55:24 +0800

    feature: start && stop

--- packages/vue-record/demo/App.vue
+++ packages/vue-record/demo/App.vue
@@ -1,13 +1,18 @@
 <!--渲染模版-->
 <template>
   <div>
-    <record></record>
+    <record
+      :minTime="2000"
+      @startRecord="startRecord"
+      @stopRecord="stopRecord"
+      @lowRecord="lowRecord"
+      ></record>
   </div>
 </template>
 
 <!--样式描述-->
 <style scoped>
-  
+
 </style>
 
 <!--组件逻辑-->
@@ -16,6 +21,17 @@
     data() {
       return {
         msg: 'Hello,Webpack'
+      }
+    },
+    methods: {
+      startRecord() {
+        console.log('开始录音')
+      },
+      stopRecord() {
+        console.log('结束录音');
+      },
+      lowRecord() {
+        console.log('录音时间过短')
       }
     }
   }
--- packages/vue-record/src/vue-record.vue
+++ packages/vue-record/src/vue-record.vue
@@ -1,84 +1,109 @@
 <template>
-    <div>
-        <div class="voice-bottom">
-            <div class="begin-shrink voice-change">
-                <div class="voice-outer">
-                    <div class="svg-voice">
-                        <voice></voice>
-                    </div>
-                </div>
-            </div>
+  <div class="voice-bottom">
+    <div class="voice-change" :class="{'begin-shrink': isRecord}">
+      <div class="voice-outer"
+        @touchstart.prevent="startRecord"
+        @touchend.prevent="stopRecord">
+        <div class="svg-voice">
+          <voice></voice>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 <script>
- import Voice from './voice.vue'
+import Voice from "./voice.vue";
 export default {
-    components: {
-      Voice
+  components: {
+    Voice
+  },
+  props: ['minTime'],
+  data() {
+    return {
+      isRecord: false,
+      beginTime: 0,
+      endTime: 0,
+      time: this.minTime || 1000
+    }
+  },
+  methods: {
+    startRecord() {
+      this.isRecord = true;
+      this.beginTime = Date.now();
+      this.$emit('startRecord');
     },
-}
+    stopRecord() {
+      this.endTime = Date.now();
+      if (this.endTime - this.beginTime < this.time) {
+        this.$emit('lowRecord');
+      } else {
+        this.$emit('stopRecord');
+      }
+      this.isRecord = false;
+
+    }
+  }
+};
 </script>
 <style>
 .voice-bottom {
-    height: 4.5rem;
-    font-size: 0.16rem;
-    text-align: center;
-    background: #fff;
-    position: absolute;
-    bottom: 0rem;
-    left: 0;
-    width: 100%;
+  height: 4.5rem;
+  font-size: 0.16rem;
+  text-align: center;
+  background: #fff;
+  position: absolute;
+  bottom: 0rem;
+  left: 0;
+  width: 100%;
 }
 
 .voice-change {
-    box-sizing: border-box;
-    margin: 0 auto;
-    height: 3rem;
-    width: 3rem;
-    border-radius: 50%;
-    background-color: #fff;
-    border: 0rem solid #fff;
-    justify-content: center;
-    align-items: center;
-    position: relative;
-    top: 1.25rem;
-    display: flex;
+  box-sizing: border-box;
+  margin: 0 auto;
+  height: 3rem;
+  width: 3rem;
+  border-radius: 50%;
+  background-color: #fff;
+  border: 0rem solid #fff;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  top: 1.25rem;
+  display: flex;
 }
 
 .voice-outer {
-    position: relative;
-    display: flex;
-    height: 1.5rem;
-    width: 1.5rem;
-    border-radius: 2rem;
-    background-color: #2ca244;
-    justify-content: center;
-    align-items: center;
+  position: relative;
+  display: flex;
+  height: 1.5rem;
+  width: 1.5rem;
+  border-radius: 2rem;
+  background-color: #2ca244;
+  justify-content: center;
+  align-items: center;
 }
 
 .svg-voice {
-    transform: rotate(19deg);
-    -webkit-transform: rotate(19deg);
+  transform: rotate(19deg);
+  -webkit-transform: rotate(19deg);
 }
 
 .begin-shrink {
-    -webkit-animation-name: circle_shrink;
-    -webkit-animation-duration: 1s;
-    -webkit-animation-timing-function: linear;
-    -webkit-animation-iteration-count: infinite;
-    background-color: #cfecd0 ;
+  -webkit-animation-name: circle_shrink;
+  -webkit-animation-duration: 1s;
+  -webkit-animation-timing-function: linear;
+  -webkit-animation-iteration-count: infinite;
+  background-color: #cfecd0;
 }
 @keyframes circle_shrink {
-    0% {
-        border:0rem solid #ffffff;
-    }
-    10% {
-        border:0.3rem solid #cfecd0;
-    }
-    100% {
-        border:0rem solid #ffffff;
-    }
+  0% {
+    border: 0rem solid #ffffff;
+  }
+  10% {
+    border: 0.3rem solid #cfecd0;
+  }
+  100% {
+    border: 0rem solid #ffffff;
+  }
 }
-    
 </style>
